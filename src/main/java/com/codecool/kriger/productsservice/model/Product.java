5bb7f45dd01ebbb2a2514e13a83373451ec58b39
gaborKriger <regirck@gmail.com>
2019-04-23 15:02:28 +0200

    refactor Product

--- src/main/java/com/codecool/kriger/productsservice/model/Product.java
+++ src/main/java/com/codecool/kriger/productsservice/model/Product.java
@@ -21,7 +21,7 @@ public class Product {
     private Long id;
     private String name;
     private String description;
-    private Double prize;
+    private Double price;
     private String imgPath;
 
 }
