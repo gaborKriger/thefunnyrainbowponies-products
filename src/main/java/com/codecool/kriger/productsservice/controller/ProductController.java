6b6dd83612fd73a01b3667509deb7a5557075cd2
gaborKriger <regirck@gmail.com>
2019-04-24 08:43:36 +0200

    create endpoints

--- src/main/java/com/codecool/kriger/productsservice/controller/ProductController.java
+++ src/main/java/com/codecool/kriger/productsservice/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.codecool.kriger.productsservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -16,15 +13,46 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @GetMapping("/")
+    public ResponseEntity root() {
+        // TODO funny
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
     @GetMapping("/products")
-    public ResponseEntity index() {
+    public ResponseEntity getProducts() {
+        // TODO error handling (if products empty...etc)
         return new ResponseEntity(productRepository.findAll(), HttpStatus.OK);
     }
 
-    @PostMapping("/product")
+    @PostMapping("/products")
     public ResponseEntity saveProduct(@RequestBody Product product) {
         productRepository.save(product);
+        // TODO error handling
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/products")
+    public ResponseEntity deleteProducts() {
+        // TODO business logic
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity getProduct(@PathVariable Long id) {
+        // TODO business logic
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity modifyProduct(@PathVariable Long id, @RequestBody Product product) {
+        // TODO business logic
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/products/{id")
+    public ResponseEntity deleteProduct(@PathVariable Long id) {
+        // TODO error handling
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
